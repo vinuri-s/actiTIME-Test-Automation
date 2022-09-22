2300f38132860e933fcf90c27776bc48b87df819
vinuri-s <piyathilakev@gmail.com>
2022-09-22 11:37:33 +0530

    Update ReportsPage.java

--- src/main/java/com/actitime/qa/pages/ReportsPage.java
+++ src/main/java/com/actitime/qa/pages/ReportsPage.java
@@ -1,15 +1,48 @@
 package com.actitime.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.actitime.qa.base.TestBase;
 
-public class ReportsPage extends TestBase  {
+import net.bytebuddy.asm.Advice.Return;
 
+public class ReportsPage extends TestBase  {
 	
-	// Web Element Xpath 
+	//Page Factory - Object Repository
 	
-	
-	// Call init
-	
-	// Mathod
-	
+			@FindBy(xpath = "//table[@id='configs']")
+			WebElement reportsList;
+			
+			@FindBy(xpath = "//table[@id='reportConfig_119']")
+			WebElement reportLink;
+			
+			@FindBy(xpath = "//div[@class='lightbox lightboxWithWhiteContent pdfPreviewLightbox createChartMode ancestorEventsEmitter verticalBar']")
+			WebElement reportDetailsSlide;
+			
+			//initialization
+			
+			public ReportsPage() {
+				
+				PageFactory.initElements(driver, this);
+			}
+			
+			
+			//Action/Methods
+			
+			public Boolean validateReportsList() {
+				return reportsList.isDisplayed();
+			}
+			
+			
+			
+			public boolean clickOnReport() {
+				
+				reportLink.click();
+				return reportDetailsSlide.isDisplayed();
+				
+			}
+			
+
 }
